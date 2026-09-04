6ea3625f32444a326f83a032975db9bc4ca538e1
witchofthewires <dcm678@gmail.com>
2026-09-03 22:34:01 -0400

    improved sine wave rendering

--- fyws.py
+++ fyws.py
@@ -1,16 +1,17 @@
 import numpy as np
 from manim import *
+import math
 
 class RealtimePlot(Scene):
 
     def construct(self):
-        axes = Axes(x_range=[0, 10], y_range=[-2, 2])
+        axes = Axes(x_range=[-10, 10], y_range=[-2, 2])
         t = ValueTracker(0)     
         curve = always_redraw(lambda: axes.plot(
             lambda x: np.sin(x - t.get_value()), 
             x_range=[0, 10], 
-            color=YELLOW
+            color=PINK
         ))
 
         self.add(axes, curve)
-        self.play(t.animate.set_value(10), run_time=5, rate_func=linear)
+        self.play(t.animate.set_value(2*math.pi), run_time=3, rate_func=linear)
